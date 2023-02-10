fc70cc81314db48c0035be06ba8d9a10b95396a1
Ramon Fernandes Viana <ramon.fv95@gmail.com>
2023-02-10 17:52:08 -0300

    feat: Trabalhando com caracteres

---
+++ sintaxe-variaveis-e-fluxo/src/TestaCaracteres.java
@@ -0,0 +1,16 @@
+public class TestaCaracteres {
+
+    public static void main(String[] args){
+
+        char letra = 'a';
+        System.out.println(letra);
+
+        String frase = "Ramon começou aprender Java ";
+        System.out.println(frase);
+
+        int ano = 2023;
+
+        System.out.println(frase + "em pleno " + ano);
+
+    }
+}
