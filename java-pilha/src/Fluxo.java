ff9bc4e9c1dc4f8679bc948169d934c744cfeb2f
Ramon Fernandes Viana <ramon.fv95@gmail.com>
2023-03-14 23:59:49 -0300

    feat: tratamento de excessoes

--- java-pilha/src/Fluxo.java
+++ java-pilha/src/Fluxo.java
@@ -1,9 +1,17 @@
+import com.sun.org.apache.bcel.internal.generic.ArithmeticInstruction;
 
 public class Fluxo {
 
     public static void main(String[] args) {
         System.out.println("Ini do main");
-        metodo1();
+        try {
+            metodo1();
+        } catch (ArithmeticException | NullPointerException ex){
+            String mensagem = ex.getMessage();
+            System.out.println("Exception " + mensagem);
+            ex.printStackTrace();
+        }
+
         System.out.println("Fim do main");
     }
 
@@ -17,6 +25,9 @@ public class Fluxo {
         System.out.println("Ini do metodo2");
         for(int i = 1; i <= 5; i++) {
             System.out.println(i);
+//            int a = i/0;
+            Conta c = null;
+            c.deposita();
         }
         System.out.println("Fim do metodo2");
     }
